9352d8209927f999bf2fb8068f1e06f730c4fabc
shubhamvitthalrasal <shubhamrasal861@gmail.com>
2024-04-21 14:45:29 +0530

    modified user js file

--- expressAuthJwt1/models/User.js
+++ expressAuthJwt1/models/User.js
@@ -5,10 +5,10 @@ const userSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
   email: { type: String, required: true, trim: true },
   password: { type: String, required: true, trim: true },
-  tc: { type: Boolean, required: true }
-})
+  tc: { type: Boolean, required: true },
+});
 
 // Model
-const UserModel = mongoose.model("user", userSchema)
+const UserModel = mongoose.model("user", userSchema);
 
-export default UserModel
+export default UserModel;
