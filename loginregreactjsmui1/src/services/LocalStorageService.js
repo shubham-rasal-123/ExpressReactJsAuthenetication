8ca14e40fb72aee2f00a51c0d998c8234e6065db
shubhamvitthalrasal <shubhamrasal861@gmail.com>
2024-04-21 14:54:34 +0530

    modified localstorageservice js file

--- loginregreactjsmui1/src/services/LocalStorageService.js
+++ loginregreactjsmui1/src/services/LocalStorageService.js
@@ -1,15 +1,14 @@
-
 const storeToken = (value) => {
-  localStorage.setItem('token', value)
-}
+  localStorage.setItem("token", value);
+};
 
 const getToken = () => {
-  let token = localStorage.getItem('token')
-  return token
-}
+  let token = localStorage.getItem("token");
+  return token;
+};
 
 const removeToken = (value) => {
-  localStorage.removeItem(value)
-}
+  localStorage.removeItem(value);
+};
 
-export { storeToken, getToken, removeToken }
+export { storeToken, getToken, removeToken };
