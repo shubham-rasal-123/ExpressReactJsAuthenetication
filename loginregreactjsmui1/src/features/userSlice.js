6ffd55759b49e48dbcef85a054e19688aa892902
shubhamvitthalrasal <shubhamrasal861@gmail.com>
2024-04-21 14:49:33 +0530

    modified userslice js file

--- loginregreactjsmui1/src/features/userSlice.js
+++ loginregreactjsmui1/src/features/userSlice.js
@@ -1,25 +1,25 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   email: "",
   name: "",
-}
+};
 
 export const userSlice = createSlice({
-  name: 'user_info',
+  name: "user_info",
   initialState,
   reducers: {
     setUserInfo: (state, action) => {
-      state.email = action.payload.email
-      state.name = action.payload.name
+      state.email = action.payload.email;
+      state.name = action.payload.name;
     },
     unsetUserInfo: (state, action) => {
-      state.email = action.payload.email
-      state.name = action.payload.name
+      state.email = action.payload.email;
+      state.name = action.payload.name;
     },
-  }
-})
+  },
+});
 
-export const { setUserInfo, unsetUserInfo } = userSlice.actions
+export const { setUserInfo, unsetUserInfo } = userSlice.actions;
 
-export default userSlice.reducer
+export default userSlice.reducer;
