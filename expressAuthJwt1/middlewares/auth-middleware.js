05ba5ac5890392512be6d15944ef8357fb8b9175
shubhamvitthalrasal <shubhamrasal861@gmail.com>
2024-04-21 14:45:03 +0530

    modified auth middleware file

--- expressAuthJwt1/middlewares/auth-middleware.js
+++ expressAuthJwt1/middlewares/auth-middleware.js
@@ -1,29 +1,31 @@
-import jwt from 'jsonwebtoken'
-import UserModel from '../models/User.js'
+import jwt from "jsonwebtoken";
+import UserModel from "../models/User.js";
 
 var checkUserAuth = async (req, res, next) => {
-  let token
-  const { authorization } = req.headers
-  if (authorization && authorization.startsWith('Bearer')) {
+  let token;
+  const { authorization } = req.headers;
+  if (authorization && authorization.startsWith("Bearer")) {
     try {
       // Get Token from header
-      token = authorization.split(' ')[1]
+      token = authorization.split(" ")[1];
 
       // Verify Token
-      const { userID } = jwt.verify(token, process.env.JWT_SECRET_KEY)
+      const { userID } = jwt.verify(token, process.env.JWT_SECRET_KEY);
 
       // Get User from Token
-      req.user = await UserModel.findById(userID).select('-password')
+      req.user = await UserModel.findById(userID).select("-password");
 
-      next()
+      next();
     } catch (error) {
-      console.log(error)
-      res.status(401).send({ "status": "failed", "message": "Unauthorized User" })
+      console.log(error);
+      res.status(401).send({ status: "failed", message: "Unauthorized User" });
     }
   }
   if (!token) {
-    res.status(401).send({ "status": "failed", "message": "Unauthorized User, No Token" })
+    res
+      .status(401)
+      .send({ status: "failed", message: "Unauthorized User, No Token" });
   }
-}
+};
 
-export default checkUserAuth
+export default checkUserAuth;
